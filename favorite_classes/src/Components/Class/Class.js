afbc98cfe3883105323d665567e9493f4198f2db
rgandhasri87 <rgandhasri87@gmail.com>
2022-05-23 23:11:18 -0700

    pulls info from api and loads onto site

--- favorite_classes/src/Components/Class/Class.js
+++ favorite_classes/src/Components/Class/Class.js
@@ -1,10 +1,41 @@
-import "./Class.css"
+import "./Class.css";
+import React, {useState, useEffect} from 'react';
 
 function Class(props) {
+
+    const [classInfo, setClassInfo] = useState({});
+    const url = "https://api.peterportal.org/rest/v0/courses/"
+
+    useEffect ( () => {
+        const fetchData = async () => {
+            const response = await fetch(url + encodeURI(props.name));
+            const data = await response.json();
+            console.log(data);
+            setClassInfo(data);
+        }
+        fetchData();
+    }, [props.name]);
     
+    // div that holds class info
+    let info;
+    if (classInfo) {
+        info =
+            <div className="classInfoContainer">
+                <em id="title">{classInfo.title}</em>
+                <p id="dept">{classInfo.department_name}</p>
+                <p id="desc">{classInfo.description}</p>
+            </div>
+    } else {
+        info = <p>Class not found!</p>
+    }
+
+
     return (
         <div className="class">
-            {props.name}
+            <strong>{props.name}</strong>
+            <div>
+                {info}
+            </div>
         </div>
     )
 }
